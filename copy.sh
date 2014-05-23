8f2910794b96c4469962505d766bdeed2d9ab1b2
Ilya Grigorik <ilya@igvita.com>
2014-05-23 15:09:08 -0700

    copy to latest table

---
+++ copy.sh
@@ -0,0 +1,4 @@
+tools/bigquery-2.0.12/bq.py cp runs.$1_pages runs.latest_pages
+tools/bigquery-2.0.12/bq.py cp runs.$1_pages_mobile runs.latest_pages_mobile
+tools/bigquery-2.0.12/bq.py cp runs.$1_requests_mobile runs.latest_requests_mobile
+tools/bigquery-2.0.12/bq.py cp runs.$1_requests runs.latest_requests
