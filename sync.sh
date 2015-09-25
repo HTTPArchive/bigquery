c2f624e91333622675a238e3c65e93ebeb2eb3bd
Ilya Grigorik <ilya@igvita.com>
2015-09-25 12:33:35 -0700

    update sync script

--- sync.sh
+++ sync.sh
@@ -12,11 +12,11 @@ if [ -n "$1" ]; then
     mobile=0
     adate=$archive
   fi
-  echo "Processing $adate, mobile: $mobile, archive: $archive" 
+  echo "Processing $adate, mobile: $mobile, archive: $archive"
 
 else
   echo "Must provide date, eg. Apr_15_2013"
-  exit 
+  exit
 fi
 
 mkdir -p $DATA/processed/$archive
@@ -25,12 +25,21 @@ echo -e "Downloading data for $archive"
 cd $DATA
 
 wget -nv -N "http://www.archive.org/download/httparchive_downloads_${adate}/httparchive_${archive}_pages.csv.gz"
+if [ $? -ne 0 ]; then
+  echo "Pages data for ${adate} is missing, exiting"
+  exit
+fi
+
 wget -nv -N "http://www.archive.org/download/httparchive_downloads_${adate}/httparchive_${archive}_requests.csv.gz"
+if [ $? -ne 0 ]; then
+  echo "Request data for ${adate} is missing, exiting"
+  exit
+fi
 
 if [ ! -f processed/${archive}/pages.csv.gz ]; then
   echo -e "Converting pages data"
   gunzip -c "httparchive_${archive}_pages.csv.gz" \
-  | sed -e 's/\\N,/"",/g' -e 's///g' -e 's/\\N$/""/g' -e's/\([^\]\)\\"/\1""/g' -e's/\([^\]\)\\"/\1""/g' -e 's/\\"","/\\\\","/g' \
+//g' -e 's/\\N$/""/g' -e's/\([^\]\)\\"/\1""/g' -e's/\([^\]\)\\"/\1""/g' -e 's/\\"","/\\\\","/g' \
   | gzip > "processed/${archive}/pages.csv.gz"
 else
   echo -e "Pages data already converted, skipping."
