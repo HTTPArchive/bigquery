16b6ef0e6007f3fdb2a35e0830bdee4473fd01c7
Ilya Grigorik <ilya@igvita.com>
2016-09-27 23:55:59 +0000

    remove old scripts

--- copy.sh
+++
@@ -1,4 +0,0 @@
-bq cp runs.$1_pages runs.latest_pages
-bq cp runs.$1_pages_mobile runs.latest_pages_mobile
-bq cp runs.$1_requests_mobile runs.latest_requests_mobile
-bq cp runs.$1_requests runs.latest_requests
