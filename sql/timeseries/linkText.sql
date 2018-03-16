a1c34083e6493c8e824e25ba99ecbb5fff782398
Rick Viscomi <rviscomi@users.noreply.github.com>
2018-03-16 16:41:57 -0400

    SEO metrics (#26)

---
+++ sql/timeseries/linkText.sql
@@ -0,0 +1,18 @@
+#standardSQL
+SELECT
+  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
+  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
+  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+  ROUND(SUM(IF(JSON_EXTRACT(report, '$.audits.link-text.score') = 'true', 1, 0)) * 100 / COUNT(0), 2) AS percent
+FROM
+  `httparchive.lighthouse.*`
+WHERE
+  report IS NOT NULL AND
+  JSON_EXTRACT(report, '$.audits.link-text.score') IS NOT NULL
+GROUP BY
+  date,
+  timestamp,
+  client
+ORDER BY
+  date DESC,
+  client
