4bece43295256712c074a05769994b3229f0ff06
tomayac <steiner.thomas@gmail.com>
2018-07-13 14:58:08 +0200

    Workbox adoption SQL

--- sql/timeseries/workboxAdoption.sql
+++ sql/timeseries/workboxAdoption.sql
@@ -1,12 +1,20 @@
 #standardSQL
 SELECT
   REGEXP_REPLACE(date, "-", "_") AS date,
-  count (uses_workboxjs) AS total_uses_workbox
+  UNIX_MILLIS(TIMESTAMP(date)) AS timestamp,
+  platform AS client,
+  0 as p10,
+  0 as p25,
+  count (uses_workboxjs) AS p50,
+  0 as p75,
+  0 as p90
 FROM
   `scratchspace.service_workers`
 WHERE
   uses_workboxjs
 GROUP BY
-  date
+  date,
+  timestamp,
+  platform
 ORDER BY
   date;
