6d43234c957ddf5dc14bd3c17e91c86773868ec4
tomayac <steiner.thomas@gmail.com>
2018-07-13 16:10:57 +0200

    Add ServiceWorkerControlledPage

---
+++ sql/timeseries/usecounterPWAs.sql
@@ -0,0 +1,18 @@
+#standardSQL
+SELECT
+  REGEXP_REPLACE(date, "-", "_") AS date,
+  UNIX_MILLIS(TIMESTAMP(date)) AS timestamp,
+  0 AS p10,
+  0 AS p25,
+  count (DISTINCT pwa_url) AS p50,
+  0 AS p75,
+  0 AS p90,
+  platform AS client
+FROM
+  `scratchspace.usecounters_pwas`
+GROUP BY
+  date,
+  timestamp,
+  platform
+ORDER BY
+  date;
