63e39e0acd24e64225f2b98f80b6970946c26472
tomayac <steiner.thomas@gmail.com>
2018-07-23 16:12:41 +0200

    Add event reports

---
+++ sql/timeseries/pushEvents.sql
@@ -0,0 +1,22 @@
+#standardSQL
+SELECT
+  REGEXP_REPLACE(date, "-", "_") AS date,
+  UNIX_MILLIS(TIMESTAMP(date)) AS timestamp,
+  platform AS client,
+  0 AS p10,
+  0 AS p25,
+  COUNT(IF (push_event,
+      TRUE,
+      NULL)) AS p50,
+  0 AS p75,
+  0 AS p90
+FROM
+  `scratchspace.service_workers`
+WHERE
+  NOT uses_workboxjs
+GROUP BY
+  date,
+  timestamp,
+  platform
+ORDER BY
+  date;
