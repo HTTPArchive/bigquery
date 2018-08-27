40b19894572c9b310c19f603aa3b9637a8011719
tomayac <steiner.thomas@gmail.com>
2018-08-27 10:57:59 +0200

    Restructure query, reformat

--- sql/timeseries/swControlledPages.sql
+++ sql/timeseries/swControlledPages.sql
@@ -1,23 +1,15 @@
 #standardSQL
 SELECT
-  REGEXP_REPLACE(date, "-", "_") AS date,
-  UNIX_MILLIS(TIMESTAMP(REGEXP_REPLACE(date, "_", "-"))) AS timestamp,
-  client,
-  ROUND(SUM(serviceworker_controlled_page) * 100 / COUNT(0), 2) AS percent
-FROM (
-  SELECT
-    IF(JSON_EXTRACT(payload,
-        '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage') IS NOT NULL,
-      1,
-      0) AS serviceworker_controlled_page,
-    REGEXP_EXTRACT(_TABLE_SUFFIX, "\\d{4}(?:_\\d{2}){2}") AS date,
-    REGEXP_EXTRACT(_TABLE_SUFFIX, ".*_(\\w+)$") AS client
-  FROM
-    `httparchive.pages.*`)
+  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
+  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
+  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+  ROUND(SUM(IF(JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage') IS NOT NULL, 1, 0)) * 100 / COUNT(0), 2) AS percent
+FROM
+  `httparchive.pages.*`
 GROUP BY
   date,
   timestamp,
   client
 ORDER BY
-  date,
+  date DESC,
   client;
