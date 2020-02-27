99b3b8b547e933c3ce78e523d13bba774996b5a7
Rick Viscomi <rviscomi@users.noreply.github.com>
2020-02-27 15:15:24 -0500

    fix SW feature query
    fixes https://github.com/HTTPArchive/httparchive.org/issues/200

--- sql/timeseries/swControlledPages.sql
+++ sql/timeseries/swControlledPages.sql
@@ -3,7 +3,10 @@ SELECT
   SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
   UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
   IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
-  ROUND(SUM(IF(JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage') IS NOT NULL, 1, 0)) * 100 / COUNT(0), 2) AS percent
+  ROUND(SUM(IF(IFNULL(
+    JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage'),
+    JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.990'))
+  IS NOT NULL, 1, 0)) * 100 / COUNT(0), 2) AS percent
 FROM
   `httparchive.pages.*`
 GROUP BY
@@ -12,4 +15,4 @@ GROUP BY
   client
 ORDER BY
   date DESC,
-  client;
+  client
