7d5f624928e3b0fbe421ec96f2240f3cc8426a2c
Rick Viscomi <rviscomi@gmail.com>
2020-08-27 13:51:05 -0400

    add lazy image metric

---
+++ sql/timeseries/imgLazy.sql
@@ -0,0 +1,16 @@
+#standardSQL
+SELECT
+  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
+  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
+  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+  ROUND(COUNT(DISTINCT IF(LOWER(attr) = '"lazy"', url, NULL)) * 100 / COUNT(0), 2) AS percent
+FROM
+  `httparchive.pages.*`,
+  UNNEST(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, "$['_img-loading-attr']"), '$')) AS attr
+GROUP BY
+  date,
+  timestamp,
+  client
+ORDER BY
+  date DESC,
+  client
