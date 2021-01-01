ab77b4612def1bb8884f75b8b95433d419ed7a54
Rick Viscomi <rviscomi@gmail.com>
2021-01-01 16:03:21 -0500

    integrate python pipeline into sync_har

--- dataflow/python/run.sh
+++ dataflow/python/run.sh
@@ -7,6 +7,6 @@ python bigquery_import.py \
   --staging_location=gs://httparchive/dataflow/staging \
   --region=us-west1 \
   --machine_type=n1-standard-32 \
-  --input android-Dec_1_2020 \
+  --input=android-Dec_1_2020 \
   --worker_disk_type=compute.googleapis.com/projects//zones//diskTypes/pd-ssd \
   --experiment=use_beam_bq_sink
