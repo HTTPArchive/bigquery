4135c7758a8e2139a9928bc44f449e21569f0b21
Barry <barry@tunetheweb.com>
2021-06-08 21:04:08 +0100

    Only FID is optional for CWV

--- sql/timeseries/cruxPassesCWV.sql
+++ sql/timeseries/cruxPassesCWV.sql
@@ -15,17 +15,17 @@ SELECT
   IF(device = 'desktop', 'desktop', 'mobile') AS client,
   SAFE_DIVIDE(
       COUNT(DISTINCT IF(
-          /* FID can be null quite often, and LCP and CLS less so */
+          /* FID can be null and is not mandatory for CWV */
           (p75_fid IS NULL OR IS_GOOD(fast_fid, avg_fid, slow_fid)) AND
-          (p75_lcp IS NULL OR IS_GOOD(fast_lcp, avg_lcp, slow_lcp)) AND
-          (p75_cls IS NULL OR IS_GOOD(small_cls, medium_cls, large_cls)), origin, NULL)),
+          IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
+          IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
       COUNT(DISTINCT origin)) * 100 AS percent
 FROM
   `chrome-ux-report.materialized.device_summary`
 WHERE
   device IN ('desktop','phone')
   AND yyyymm > 201909
-  AND (p75_fid IS NOT NULL OR p75_lcp IS NOT NULL OR p75_cls IS NOT NULL) /* Must have at least one CWV */
+  AND p75_lcp IS NOT NULL AND p75_cls IS NOT NULL /* Must have LCP and CLS */
 GROUP BY
   date,
   timestamp,
