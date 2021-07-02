29c72f903cfbdf3cf9a451c6f549f29293ea208c
Barry <barry@tunetheweb.com>
2021-07-02 23:41:16 +0100

    Stop a11yscore crashing on large lighthouse payloads

--- sql/timeseries/a11yScores.sql
+++ sql/timeseries/a11yScores.sql
@@ -1,12 +1,12 @@
 #standardSQL
-CREATE TEMPORARY FUNCTION getA11yScore(report STRING)
+CREATE TEMPORARY FUNCTION getA11yScore(reportCategories STRING, score STRING)
 RETURNS FLOAT64
 LANGUAGE js AS """
-  $=JSON.parse(report);
-  if ($.reportCategories) {
-    return $.reportCategories.find(i => i.name === 'Accessibility').score;
+  if (reportCategories) {
+    $=JSON.parse(reportCategories);
+    return $.find(i => i.name === 'Accessibility').score;
   } else {
-    return $.categories.accessibility.score * 100;
+    return score * 100;
   }
 """;
 
@@ -14,11 +14,11 @@ SELECT
   SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
   UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
   IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
-  ROUND(APPROX_QUANTILES(getA11yScore(report), 101)[OFFSET(11)], 2) AS p10,
-  ROUND(APPROX_QUANTILES(getA11yScore(report), 101)[OFFSET(26)], 2) AS p25,
-  ROUND(APPROX_QUANTILES(getA11yScore(report), 101)[OFFSET(51)], 2) AS p50,
-  ROUND(APPROX_QUANTILES(getA11yScore(report), 101)[OFFSET(76)], 2) AS p75,
-  ROUND(APPROX_QUANTILES(getA11yScore(report), 101)[OFFSET(91)], 2) AS p90
+  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 101)[OFFSET(11)], 2) AS p10,
+  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 101)[OFFSET(26)], 2) AS p25,
+  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 101)[OFFSET(51)], 2) AS p50,
+  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 101)[OFFSET(76)], 2) AS p75,
+  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 101)[OFFSET(91)], 2) AS p90
 FROM
   `httparchive.lighthouse.*`
 WHERE
