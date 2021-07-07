8ddc5e6f138587b58c46bc7d991efa58d559b301
Barry <barry@tunetheweb.com>
2021-07-07 22:01:24 +0100

    Review feedback + add PWA optimisation

--- sql/timeseries/a11yScores.sql
+++ sql/timeseries/a11yScores.sql
@@ -1,29 +1,33 @@
 #standardSQL
-# Lighthouse changed format of scores in v3.0.0 released in July 2018 so hande old and new
-CREATE TEMPORARY FUNCTION getA11yScore(reportCategories STRING, score STRING)
-RETURNS FLOAT64
+# Lighthouse changed format of scores in v3.0.0 released in July 2018 so handle old with a UDF
+CREATE TEMPORARY FUNCTION getA11yScore(reportCategories STRING)
+RETURNS FLOAT64 DETERMINISTIC
 LANGUAGE js AS """
-  if (reportCategories) {
-    $=JSON.parse(reportCategories);
+  $=JSON.parse(reportCategories);
+  if($) {
     return $.find(i => i.name === 'Accessibility').score;
-  } else {
-    return score * 100;
   }
 """;
 
 SELECT
-  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
-  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
-  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
-  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 1000)[OFFSET(100)], 2) AS p10,
-  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 1000)[OFFSET(250)], 2) AS p25,
-  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 1000)[OFFSET(500)], 2) AS p50,
-  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 1000)[OFFSET(750)], 2) AS p75,
-  ROUND(APPROX_QUANTILES(getA11yScore(JSON_EXTRACT(report, '$.reportCategories'), JSON_EXTRACT(report, '$.categories.accessibility.score')), 1000)[OFFSET(900)], 2) AS p90
-FROM
-  `httparchive.lighthouse.*`
-WHERE
-  report IS NOT NULL
+  date,
+  UNIX_DATE(CAST(REPLACE(date, '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
+  client,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(100)], 2) AS p10,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(250)], 2) AS p25,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(500)], 2) AS p50,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(750)], 2) AS p75,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(900)], 2) AS p90
+FROM (
+  SELECT
+    SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
+    IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+    IFNULL(CAST(JSON_EXTRACT(report, '$.categories.accessibility.score') AS FLOAT64) * 100,getA11yScore(JSON_EXTRACT(report, '$.reportCategories'))) AS score
+  FROM
+    `httparchive.lighthouse.*`
+  WHERE
+    report IS NOT NULL
+)
 GROUP BY
   date,
   timestamp,
--- sql/timeseries/pwaScores.sql
+++ sql/timeseries/pwaScores.sql
@@ -1,30 +1,35 @@
 #standardSQL
-CREATE TEMPORARY FUNCTION getPWAScore(report STRING)
+# Lighthouse changed format of scores in v3.0.0 released in July 2018 so handle old with a UDF
+CREATE TEMPORARY FUNCTION getPWAScore(reportCategories STRING)
 RETURNS FLOAT64
 LANGUAGE js AS """
-  $=JSON.parse(report);
-  if ($.reportCategories) {
-    return $.reportCategories.find(i => i.name === 'Progressive Web App').score;
-  } else {
-    return $.categories.pwa.score * 100;
+  $=JSON.parse(reportCategories);
+  if ($) {
+    return $.find(i => i.name === 'Progressive Web App').score;
   }
 """;
 
 SELECT
-  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
-  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
-  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
-  ROUND(APPROX_QUANTILES(getPWAScore(report), 101)[OFFSET(11)], 2) AS p10,
-  ROUND(APPROX_QUANTILES(getPWAScore(report), 101)[OFFSET(26)], 2) AS p25,
-  ROUND(APPROX_QUANTILES(getPWAScore(report), 101)[OFFSET(51)], 2) AS p50,
-  ROUND(APPROX_QUANTILES(getPWAScore(report), 101)[OFFSET(76)], 2) AS p75,
-  ROUND(APPROX_QUANTILES(getPWAScore(report), 101)[OFFSET(91)], 2) AS p90
-FROM
-  `httparchive.lighthouse.*`
-WHERE
-  report IS NOT NULL
-  AND (JSON_EXTRACT(report, "$.audits.service-worker.score") = 'true'
-    OR JSON_EXTRACT(report, "$.audits.service-worker.score") = '1')
+  date,
+  UNIX_DATE(CAST(REPLACE(date, '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
+  client,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(100)], 2) AS p10,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(250)], 2) AS p25,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(500)], 2) AS p50,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(750)], 2) AS p75,
+  ROUND(APPROX_QUANTILES(score, 1000)[OFFSET(900)], 2) AS p90
+FROM (
+  SELECT
+    SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
+    IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+    IFNULL(CAST(JSON_EXTRACT(report, '$.categories.pwa.score') AS FLOAT64) * 100,getPWAScore(JSON_EXTRACT(report, '$.reportCategories'))) AS score
+  FROM
+    `httparchive.lighthouse.*`
+  WHERE
+    report IS NOT NULL
+    AND (JSON_EXTRACT(report, "$.audits.service-worker.score") = 'true'
+         OR JSON_EXTRACT(report, "$.audits.service-worker.score") = '1')
+)
 GROUP BY
   date,
   timestamp,
