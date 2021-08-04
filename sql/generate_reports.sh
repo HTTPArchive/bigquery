14dc38863c956e5cd43c8bffb52d0e28cdc067e6
Barry <barry@tunetheweb.com>
2021-08-04 13:02:51 +0100

    Limit force run dates

--- sql/generate_reports.sh
+++ sql/generate_reports.sh
@@ -190,6 +190,24 @@ else
 						date_join="${date_join} AND SUBSTR(_TABLE_SUFFIX, 0, 10) <= \"$YYYY_MM_DD\""
 					fi
 				fi
+			else
+				# Even if doing a force run we only wanna run up until date given in case next month is mid-run as don't wanna get just desktop data
+				if [[ $(grep "httparchive.blink_features.usage" $query) && $LENS == "" ]]; then # blink needs a special join, different for lenses
+					if [[ -n "$YYYY_MM_DD" ]]; then
+						# If a date is given, then only run up until then (in case next month is mid-run as don't wanna get just desktop data)
+						date_join="yyyymmdd <= REPLACE(\"$YYYY_MM_DD\",\"_\",\"\")"
+					fi
+				elif [[ $(grep "httparchive.blink_features.usage" $query) && $LENS != "" ]]; then # blink needs a special join, different for lenses
+					if [[ -n "$YYYY_MM_DD" ]]; then
+						# If a date is given, then only run up until then (in case next month is mid run as don't wanna get just desktop data)
+						date_join="yyyymmdd <= CAST(REPLACE(\"$YYYY_MM_DD\",\"_\",\"-\") AS DATE)"
+					fi
+				elif [[ $metric != crux* ]]; then # CrUX is quick and join is more compilicated so just do a full run of that
+					if [[ -n "$YYYY_MM_DD" ]]; then
+						# If a date is given, then only run up until then (in case next month is mid run as don't wanna get just desktop data)
+						date_join="SUBSTR(_TABLE_SUFFIX, 0, 10) <= \"$YYYY_MM_DD\""
+					fi
+				fi
 			fi
 		else
 			# Even if the file doesn't exist we only wanna run up until date given in case next month is mid-run as don't wanna get just desktop data
