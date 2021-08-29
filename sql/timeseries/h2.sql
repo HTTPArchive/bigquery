0face1c26fcd10a0db64f995a2644f25e630bad7
Barry <barry@tunetheweb.com>
2021-08-29 16:53:18 +0100

    Improvements to Generate Script

--- sql/timeseries/h2.sql
+++ sql/timeseries/h2.sql
@@ -5,7 +5,16 @@ SELECT
   IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
   ROUND(SUM(IF(protocol = 'HTTP/2', 1, 0)) * 100 / COUNT(0), 2) AS percent
 FROM
-  (SELECT page AS url, JSON_EXTRACT_SCALAR(payload, '$._protocol') AS protocol, _TABLE_SUFFIX AS _TABLE_SUFFIX FROM `httparchive.requests.*`)
+  (
+    SELECT
+      page AS url,
+      JSON_EXTRACT_SCALAR(payload, '$._protocol') AS protocol,
+      _TABLE_SUFFIX AS _TABLE_SUFFIX
+    FROM
+      `httparchive.requests.*`
+    WHERE
+      1=1 /* Dummy join to allow date joins on this subquery for cost/time savings */
+  )
 GROUP BY
   date,
   timestamp,
