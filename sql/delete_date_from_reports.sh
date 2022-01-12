6e61a1ce561f205f2b3e133888d4a2ff59130122
Barry <barry@tunetheweb.com>
2022-01-12 19:17:01 +0000

    Add script to delete one data from timeseries

---
+++ sql/delete_date_from_reports.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# Removes a particular date JSON from timeseries reports on Google Storage.
+#
+# Usage:
+#
+#   $ sql/delete_date_from_reports.sh -d YYYY_MM_DD
+#   $ sql/delete_date_from_reports.sh -d YYYY_MM_DD -l top1k
+#   $ sql/delete_date_from_reports.sh -d YYYY_MM_DD -l top1k -r "*crux*"
+#
+# Flags:
+#
+#   -l: Optional name of the report lens to generate, eg "top10k".
+#
+#   -r: Optional name of the report files to generate, eg "*crux*".
+#
+
+set -o pipefail
+
+FORCE=0
+LENS_ARG=""
+REPORTS="*"
+
+# Read the flags.
+while getopts ":d:l:r:" opt; do
+	case "${opt}" in
+		d)
+			YYYY_MM_DD=${OPTARG}
+			;;
+		l)
+			LENS_ARG=${OPTARG}
+			;;
+		r)
+			REPORTS=${OPTARG}
+			;;
+	esac
+done
+
+if [[ "${YYYY_MM_DD}" == "" ]]; then
+  echo "Usage $0 -d 2021_12_01"
+  exit 1
+fi
+
+echo "${YYYY_MM_DD}"
+
+# Run all timeseries queries.
+for query in sql/timeseries/$REPORTS.sql; do
+
+	if [[ ! -f $query ]]; then
+		echo "Nothing to do"
+		continue;
+	fi
+
+	# Extract the metric name from the file path.
+	metric=$(echo $(basename $query) | cut -d"." -f1)
+
+	if [[ "${LENS_ARG}" == "" ]]; then
+		LENSES=("")
+		echo "Deleting ${metric} report for base"
+	elif [[ "${LENS_ARG}" == "ALL" ]]; then
+		LENSES=("" $(ls sql/lens))
+		echo "Deleting ${metric} report for base and all lenses"
+	else
+		LENSES=("${LENS_ARG}")
+		echo "Deleting ${metric} report for one lens"
+	fi
+
+	for LENS in "${LENSES[@]}"
+	do
+
+		gs_lens_dir=""
+		if [[ $LENS != "" ]]; then
+			gs_lens_dir="$LENS/"
+		fi
+
+		current_contents=""
+		gs_url="gs://httparchive/reports/$gs_lens_dir${metric}.json"
+		gsutil ls $gs_url &> /dev/null
+
+		if [ $? -eq 0 ]; then
+
+			echo "Updating this query: ${sql}\n"
+
+			# The file exists, so remove the requested date
+			current_contents=$(gsutil cat $gs_url)
+			new_contents=$(echo "$current_contents" | jq -c --indent 1 '.[] | select(.date!="$YYY_MM_DD")' | tr -d '\n' | sed 's/^/[ /' | sed 's/}$/ } ]\n/' | sed 's/}{/ }, {/g')
+
+			# Make sure the removal succeeded.
+			if [ $? -eq 0 ]; then
+
+				# Upload the response to Google Storage.
+				echo $new_contents \
+					| gsutil  -h "Content-Type:application/json" cp - $gs_url
+			else
+				echo $new_contents >&2
+			fi
+		fi
+	done
+done
+
+echo -e "Done"
