a62c7ad3e7abb1b677bfffbdb92c1b186ae907a5
Barry Pollard <barry@tunetheweb.com>
2024-04-04 15:20:10 +0100

    Update INP from experimental

--- sql/histograms/cruxInp.sql
+++ sql/histograms/cruxInp.sql
@@ -14,7 +14,7 @@ FROM (
     FROM (
       SELECT
         form_factor,
-        experimental.interaction_to_next_paint.histogram.bin AS bins
+        interaction_to_next_paint.histogram.bin AS bins
       FROM
         `chrome-ux-report.all.${YYYYMM}`
     )
