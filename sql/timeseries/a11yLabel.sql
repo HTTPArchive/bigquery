cd0d1c7e207a4e4a57e4e980c8832f51b334b0ce
Barry Pollard <barrypollard@google.com>
2025-01-13 16:26:35 +0000

    Fix two reports

--- sql/timeseries/a11yLabel.sql
+++ sql/timeseries/a11yLabel.sql
@@ -3,12 +3,12 @@ SELECT
   FORMAT_TIMESTAMP('%Y_%m_%d', date) AS date,
   UNIX_DATE(date) * 1000 * 60 * 60 * 24 AS timestamp,
   client,
-  ROUND(SUM(IF(LAX_STRING(lighthouse.audits,label.score) IN ('true', '1'), 1, 0)) * 100 / COUNT(0), 2) AS percent
+  ROUND(SUM(IF(LAX_STRING(lighthouse.audits.label.score) IN ('true', '1'), 1, 0)) * 100 / COUNT(0), 2) AS percent
 FROM
   `httparchive.crawl.pages`
 WHERE
   /* Should really use the following to only include eligible sites. */
-  /* LAX_STRING(lighthouse.audits,label.score) IS NOT NULL AND */
+  /* LAX_STRING(lighthouse.audits.label.score) IS NOT NULL AND */
   lighthouse IS NOT NULL AND
   TO_JSON_STRING(lighthouse) != '{}' AND
   date >= '2017-06-01' AND
