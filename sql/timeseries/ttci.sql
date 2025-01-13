15d8bcf5385bea39454970e0fc6589f3c36b6060
Barry Pollard <barrypollard@google.com>
2025-01-13 22:22:20 +0000

    Missed reports

--- sql/timeseries/ttci.sql
+++ sql/timeseries/ttci.sql
@@ -1,8 +1,8 @@
 #standardSQL
 SELECT
-  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
-  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
-  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
+  FORMAT_TIMESTAMP('%Y_%m_%d', date) AS date,
+  UNIX_DATE(date) * 1000 * 60 * 60 * 24 AS timestamp,
+  client,
   ROUND(APPROX_QUANTILES(value, 1000)[OFFSET(100)], 2) AS p10,
   ROUND(APPROX_QUANTILES(value, 1000)[OFFSET(250)], 2) AS p25,
   ROUND(APPROX_QUANTILES(value, 1000)[OFFSET(500)], 2) AS p50,
@@ -10,18 +10,20 @@ SELECT
   ROUND(APPROX_QUANTILES(value, 1000)[OFFSET(900)], 2) AS p90
 FROM (
   SELECT
-    _TABLE_SUFFIX AS _TABLE_SUFFIX,
-    CAST(IFNULL(
-      JSON_EXTRACT(report, '$.audits.interactive.numericValue'),
+    client,
+    date,
+    IFNULL(
+      FLOAT64(lighthouse.audits.interactive.numericValue),
       IFNULL(
-        JSON_EXTRACT(report, '$.audits.interactive.rawValue'),
-        JSON_EXTRACT(report, '$.audits.consistently-interactive.rawValue')
+        FLOAT64(lighthouse.audits.interactive.rawValue),
+        FLOAT64(lighthouse.audits['consistently-interactive].rawValue)
       )
-    ) AS FLOAT64) / 1000 AS value
+    ) / 1000 AS value
   FROM
-    `httparchive.lighthouse.*`
+    `httparchive.crawl.pages`
   WHERE
-    _TABLE_SUFFIX < '2022_03_01'
+    is_root_page AND
+    date >= '2016-01-01'
 )
 GROUP BY
   date,
